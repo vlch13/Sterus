b25e4fdf247b29598eb2abab6ae1f1128db373d6
Vladimir Chudin <vl.chudin@gmail.com>
2023-06-09 17:42:27 +0300

    Fix Product.BoxWeight type

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -6,7 +6,7 @@ namespace Core.Entities
 		public int BoxLenght { get; set; }
 		public int BoxHeight { get; set; }
 		public int BoxWidth { get; set; }
-		public int BoxWeight { get; set; }
+		public double BoxWeight { get; set; }
 		public int Dose { get; set; }
 		public string DoseControl { get; set; }
 		public double Speed { get; set; }
