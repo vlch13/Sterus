2e9e517bf9a3d0ad49d606be00d0c8106e527da5
Vladimir Chudin <vl.chudin@gmail.com>
2023-06-15 21:20:30 +0300

    Add nav-bar + add Product & Pagination interfaces

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Sterus';
+export class AppComponent implements OnInit {
+	title = 'Sterus';
+	products: Product[] = [];
+
+	constructor(private http: HttpClient) { }
+
+	ngOnInit(): void {
+		this.http.get<Pagination<Product[]>>('https://localhost:5005/api/products?pageSize=50').subscribe({
+			next: response => this.products = response.data, //what ot do next
+			error: error => console.log(error), //what to do of error
+			complete: () => {
+				console.log('Request completed');
+				console.log('Extra statment');
+			}
+		})
+	}
 }
---
+++ client/src/app/models/product.ts
@@ -0,0 +1,14 @@
+export interface Product {
+	productCompany: string
+	name: string
+	boxLenght: number
+	boxHeight: number
+	boxWidth: number
+	boxWeight: number
+	dose: number
+	doseControl: string
+	speed: number
+	isMedical: boolean
+	price: number
+	note: string
+}
