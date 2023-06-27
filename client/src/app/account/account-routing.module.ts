e0a9517f3d26a56bd4a2f57d6a4724b7f980d19f
Vladimir Chudin <vl.chudin@gmail.com>
2023-06-27 13:13:42 +0300

    Add AuthGuard to register form

--- client/src/app/account/account-routing.module.ts
+++ client/src/app/account/account-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 const routes: Routes = [
 	{ path: 'login', component: LoginComponent },
-	{ path: 'register', component: RegisterComponent },
+	{ path: 'register', canActivate: [AuthGuard], component: RegisterComponent },
 ]
 
 @NgModule({
